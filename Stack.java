67b732334c57e4d8f47088eef419703530b8a785
Jonathan Silal <jsilal@albany.edu>
2016-10-30 12:52:54 -0400

    default constructor Stack made

---
+++ Stack.java
@@ -0,0 +1,40 @@
+public class Stack{
+ //First node
+ private Node head;
+
+ //Last node
+ private Node tail;
+
+ private int nodeCount;
+
+ //Default Constructor - set NodeCount along with first and last (null)
+ public Stack()
+
+ {
+  nodeCount = 0;
+  head = null;
+ }
+
+ //Get the size of the list, and returns the interger size of the list
+ public int size()
+ {
+  return nodeCount;
+ }
+
+ //Checks to see if the list is empty or not, returns boolean - empty = true
+ public boolean isEmpty()
+ {
+  return (head != null);
+ }
+
+ //Prints the list forward from first
+ public void printForward()
+ {
+  Node seek = head;
+  while (seek != null)
+  {
+   System.out.println("  " + seek.getData());
+   seek = seek.getNext();
+  }
+ }
+
