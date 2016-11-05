d6372631955fc67d4690b8338778ae140aeeb4f9
Jonathan Silal <jsilal@albany.edu>
2016-11-05 15:52:21 -0400

    Method made to test           Queue to a Stack and Stack to a Stack

--- TestQ.java
+++ TestQ.java
@@ -1,5 +1,18 @@
 public class TestQ
 {
+	public static void main(String args[])
+		{
+			System.out.println("ST------------------");
+			StackTester();
+			System.out.println("QT------------------");
+			QueueTester();
+			System.out.println("SQ------------------");
+			StacktoQueue();
+			System.out.println("QS------------------");
+			QueuetoStack();
+			System.out.println("SS------------------");
+			StacktoStack();
+		}
 	public static void StackTester()
 	{
 		Stack s = new Stack();
@@ -29,5 +42,62 @@ public static void DequeueAll(Queue q)
 		{
 			System.out.println(q.dequeue());
 		}
+	}
+	public static void StackStack(Stack s1, Stack s2)
+		{
+			while (s1.peek()!= null)
+			{
+				s2.push(s1.pop());
+			}
+		}
+
+		public static void StacktoQueue()
+		{
+			Stack s = new Stack();
+			Queue q = new Queue();
+			s.push("John");
+			s.push("Mike");
+			s.push("Kevin");
+			// Go from stack to queue
+			while (s.peek() != null)
+			{
+				q.enqueue(s.pop());
+			}
+			//print out queue
+			DequeueAll(q);
+	}
+	public static void QueuetoStack()
+
+	{
+			Stack s = new Stack();
+			Stack s2 = new Stack();
+			Queue q = new Queue();
+
+			q.enqueue("John");
+			q.enqueue("Mike");
+			q.enqueue("Kevin");
+			// Go from queue to stack
+			while (q.peek() != null)
+			{
+				s.push(q.dequeue());
+			}
+			StackStack(s,s2);
+			//print out queue
+			PopStack(s2);
+		}
+		public static void StacktoStack()
+
+		{
+				Stack s = new Stack();
+				Stack s2 = new Stack();
+
+
+				s.push("John");
+				s.push("Mike");
+				s.push("Kevin");
+				StackStack(s,s2);
+				StackStack(s2,s);
+				//print out queue
+				PopStack(s);
 	}
 }
